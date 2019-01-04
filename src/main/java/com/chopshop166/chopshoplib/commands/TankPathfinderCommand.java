66b3a9b0aedfe158696c83d85caf6db458c0af69
Matt Soucy <msoucy@csh.rit.edu>
2019-01-03 23:14:31 -0500

    Document new classes

--- src/main/java/com/chopshop166/chopshoplib/commands/TankPathfinderCommand.java
+++ src/main/java/com/chopshop166/chopshoplib/commands/TankPathfinderCommand.java
@@ -18,28 +18,62 @@ public final class TankPathfinderCommand extends Command {
     private EncoderFollower leftEnc;
     private EncoderFollower rightEnc;
 
+    /**
+     * Build a {@link TankPathfinderCommand} from individual {@link Waypoint}s.
+     */
     public static class Builder {
         private final Trajectory.Config config;
         private final List<Waypoint> waypoints = new ArrayList<>();
 
+        /**
+         * Store the config used to generate the {@link Trajectory}.
+         * 
+         * @param config The config to be stored.
+         */
         public Builder(final Trajectory.Config config) {
             this.config = config;
         }
 
+        /**
+         * Add a {@link Waypoint} to the path.
+         * 
+         * @param waypoint The next location to move to.
+         * @return {@code this} for chaining.
+         */
         public Builder then(final Waypoint waypoint) {
             waypoints.add(waypoint);
             return this;
         }
 
+        /**
+         * Add a {@link Waypoint} to the path.
+         * 
+         * @param x     The next x coordinate to move to.
+         * @param y     The next y coordinate to move to.
+         * @param angle The next exit angle.
+         * @return {@code this} for chaining.
+         */
         public Builder then(final double x, final double y, final double angle) {
             return then(new Waypoint(x, y, angle));
         }
 
+        /**
+         * Clear all {@link Waypoint}s from the path.
+         * 
+         * @return {@code this} for chaining.
+         */
         public Builder clear() {
             waypoints.clear();
             return this;
         }
 
+        /**
+         * Create a {@link Command} from the provided path.
+         * 
+         * @param name      The name of the command.
+         * @param subsystem The Tank Drive to use for driving.
+         * @return The created command.
+         */
         public TankPathfinderCommand compile(final String name, final TankDriveSubsystem subsystem) {
             final Waypoint[] points = waypoints.toArray(new Waypoint[0]);
             final Trajectory trajectory = Pathfinder.generate(points, config);
@@ -54,6 +88,14 @@ public final class TankPathfinderCommand extends Command {
         }
     }
 
+    /**
+     * Create a Command.
+     * 
+     * @param name      The name of the command.
+     * @param subsystem The drive subsystem to operate on.
+     * @param leftEnc   The left encoder follower.
+     * @param rightEnc  The right encoder follower.
+     */
     protected TankPathfinderCommand(String name, TankDriveSubsystem subsystem, EncoderFollower leftEnc,
             EncoderFollower rightEnc) {
         super(name, subsystem);
--- src/main/java/com/chopshop166/chopshoplib/outputs/TankDriveSubsystem.java
+++ src/main/java/com/chopshop166/chopshoplib/outputs/TankDriveSubsystem.java
@@ -9,21 +9,72 @@ import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.followers.EncoderFollower;
 import jaci.pathfinder.modifiers.TankModifier;
 
+/**
+ * A {@link Subsystem} that contains information for a Tank Drive.
+ */
 public abstract class TankDriveSubsystem extends Subsystem {
+    /**
+     * Get the drive train from the robot.
+     * <p>
+     * Assume that the subsystem is using the built in WPIlib utilities.
+     * 
+     * @return The drive train.
+     */
     public abstract DifferentialDrive getDriveTrain();
 
+    /**
+     * Get the left encoder value.
+     * 
+     * @return The left encoder value.
+     */
     public abstract Integer getLeftEncoder();
 
+    /**
+     * Get the right encoder value.
+     * 
+     * @return The right encoder value.
+     */
     public abstract Integer getRightEncoder();
 
+    /**
+     * Configure the left {@link EncoderFollower} for this particular drive train.
+     * 
+     * @param follower The follower to modify.
+     */
     public abstract void configureLeftEncoderFollower(EncoderFollower follower);
 
-    public abstract void configureRightEncoderFollower(EncoderFollower follower);
+    /**
+     * Configure the right {@link EncoderFollower} for this particular drive train.
+     * <p>
+     * Assumes the same values as the left by default.
+     * 
+     * @param follower The follower to modify.
+     */
+    public void configureRightEncoderFollower(EncoderFollower follower) {
+        configureLeftEncoderFollower(follower);
+    }
 
+    /**
+     * Get the {@link Gyro} instance on the robot.
+     * 
+     * @return The {@link Gyro} in question.
+     */
     public abstract Gyro getGyro();
 
+    /**
+     * Get a modifier for a {@link Trajectory} for this particular drive train.
+     * 
+     * @param trajectory The source trajectory.
+     * @return A modifier for this drive train.
+     */
     public abstract TankModifier getModifier(Trajectory trajectory);
 
+    /**
+     * Convenience function to create a builder.
+     * 
+     * @param config The configuration for the path.
+     * @return The builder object.
+     */
     public TankPathfinderCommand.Builder path(Trajectory.Config config) {
         return new TankPathfinderCommand.Builder(config);
     }
